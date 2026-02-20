e7858fbc1e9a20271497cc5d8c6f3934dcf0f01d
danielmeco <danielmeco20@gmail.com>
2026-02-20 12:19:47 +0100

    build(gradle): created java spring boot project for Lobby Service

---
+++ LobbyService/src/test/java/it/unibo/lobbyservice/LobbyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unibo.lobbyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
